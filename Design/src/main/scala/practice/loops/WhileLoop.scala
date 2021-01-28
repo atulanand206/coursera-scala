97897cf8809f2be74a4691b05e3e8f84c019596b
Atul Anand <atulanand206@gmail.com>
2021-01-28 21:01:41 +0530

    ✍ While and repeat using higher order functions.

--- Design/src/main/scala/practice/loops/WhileLoop.scala
+++ Design/src/main/scala/practice/loops/WhileLoop.scala
@@ -2,7 +2,7 @@ package practice.loops
 
 import scala.annotation.tailrec
 
-object WhileLoop {
+object WhileLoop extends App {
 
   @tailrec
   def WHILE(condition: => Boolean)(command: => Unit): Unit = {
@@ -12,10 +12,17 @@ object WhileLoop {
     } else ()
   }
 
+  def equalsToZero(): Boolean = 0 == 0
+  def equalsToOne(): Boolean = 0 == 1
+  def print() : Unit = println("A")
+//  WHILE(equalsToZero())(print())
+
   @tailrec
   def REPEAT(condition: => Boolean)(command: => Unit): Unit = {
     command
     if (condition) ()
     else REPEAT(condition)(command);
   }
+
+  REPEAT(equalsToZero())(print())
 }
